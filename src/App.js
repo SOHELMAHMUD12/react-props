fff860717b01e491dc5c84fe5c3a8f56344c099e
SOHELMAHMUD12 <sohelcse123@gmail.com>
2022-09-20 12:01:03 +0600

    added array object

--- src/App.js
+++ src/App.js
@@ -2,16 +2,37 @@ import logo from './logo.svg';
 import './App.css';
 
 
+const singers =[
+  {name:'raihan', job:'singer'},
+  {name:'murad', job:'singer'},
+  {name:'rahat', job:'singer'}
+]
+
+
 function App() {
+
+const names =['sohel', 'rashed', 'abrar', 'mabrur','hasan', 'kamal'];
   return (
     <div className="App">
-      <Person name="Sohel Mahmud" study="najera" phone="01911173514" id="1250"></Person>
-      <Person name="Sohel Mahmud" study="play" phone="01822218496" id="1250"></Person>
+      {
+        names.map(name => <li> Name: {name}</li>)
+      }
+
+      {
+        names.map(name => <Person name={name}></Person> )
+      }
+
+      {
+        singers.map(singer => <Person name ={singers.name}></Person>)
+      }
+      {/* <Person name ={names[0]} phone="01545254"></Person>
+      <Person name={names[1]} study="najera" phone="01911173514" id="1250"></Person>
+      <Person name={names[2]} study="play" phone="01822218496" id="1250"></Person> */}
       <p> New Component</p>
-      <Friend phone="01834077500" id="1234"></Friend>
+      <Friend name ="Jamsade kamal" phone="01834077500" id="1234"></Friend>
       <Friend phone="01834077500" id="1234"></Friend>
       <p>New Component</p>
-      <Vai phone="01673342840" id="123456"></Vai>
+      <Vai name = " Nazmul Hasan"phone="01673342840" id="123456"></Vai>
       <Vai  phone="01673342840" id="123456"></Vai>
     </div>
   );
@@ -21,8 +42,7 @@ function Person(props){
   console.log(props);
   return (
     <div className='person'>
-       <h3>Name:Sohel Mahmud</h3>
-      <p>Profession:Front End Developer</p>
+      
      <h1>{props.name}</h1>
      <p>{props.study}</p>
      <p>{props.phone}</p>
